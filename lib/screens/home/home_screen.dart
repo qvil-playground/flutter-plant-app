fa9bd622248cadd82ae84fbb2954a1faa5b369bc
Taesu Hyeon <qvil1127@gmail.com>
2020-08-24 01:50:05 +0900

    Complete MyBottomNavBar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:plant_app/screens/home/widgets/body.dart';
+import 'package:plant_app/widgets/my_bottom_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -13,6 +14,7 @@ class HomeScreen extends StatelessWidget {
             icon: SvgPicture.asset("assets/icons/menu.svg"), onPressed: () {}),
       ),
       body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
     );
   }
 }
--- lib/screens/home/widgets/body.dart
+++ lib/screens/home/widgets/body.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
 
+import 'featured_plant.dart';
 import 'header_with_search_box.dart';
+import 'recomend_plant.dart';
+import 'title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -8,7 +12,22 @@ class Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
       child: Column(
-        children: <Widget>[HeaderWithSearchBox(size: size)],
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(
+            title: "Recomended",
+            press: () {},
+          ),
+          RecomendsPlants(),
+          TitleWithMoreBtn(
+            title: "Featured Plants",
+            press: () {},
+          ),
+          FeaturedPlants(),
+          SizedBox(
+            height: kDefaultPadding,
+          )
+        ],
       ),
     );
   }
